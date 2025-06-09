def2dbd00e9d61dfed97dda10861cd8315707e75
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2025-06-09 15:41:25 -0400

    RDKEMW-4128 : Coverity integration

--- stubs/dshal_stubs.c
+++ stubs/dshal_stubs.c
@@ -9,8 +9,6 @@
 #include "dsUtl.h"
 #include "dsError.h"
 
-#define RDK_DSHAL_NAME "libdshal.so" 
-
 dsError_t dsGetDisplay(dsVideoPortType_t m_vType, int index, intptr_t *handle)
 {
     return dsERR_NONE;
