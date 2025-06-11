6d96e5e534bedca6d0034825940ba9f8cbf8fad4
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2025-06-11 12:33:11 -0400

    RDKEMW-4128 Coverity Integration

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -74,7 +74,7 @@ export DS_PATH=$ROOT/devicesettings
 
 cd $ROOT
 rm -rf rdk-halif-device_settings
-git clone --branch 5.1.0 --depth 1 https://github.com/rdkcentral/rdk-halif-device_settings.git
+git clone --branch 6.0.0 --depth 1 https://github.com/rdkcentral/rdk-halif-device_settings.git
 export DS_IF_PATH=$ROOT/rdk-halif-device_settings
 
 cd $WORKDIR
