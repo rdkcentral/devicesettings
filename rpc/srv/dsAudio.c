6d6c074993d6b6828556e45a9f8b8aea73d9333f
apatel859 <Amit_Patel5@comcast.com>
2026-07-22 16:04:41 +0000

    RDKEMW-22129: set volume properly for FFV
    Signed-off-by: apatel859 <Amit_Patel5@comcast.com>

--- rpc/srv/dsAudio.c
+++ rpc/srv/dsAudio.c
@@ -419,6 +419,8 @@ void AudioConfigInit()
                         INT_INFO("Port %s: Initialized audio level : %f\n","HDMI0", m_audioLevel);
                     }
                 }
+		m_LastVolumeLevel = m_audioLevel; 
+               INT_INFO("%s: audio level during init config m_LastVolumeLevel : %f\n", __FUNCTION__, float(m_LastVolumeLevel));
 		
             }
             else {
@@ -2911,36 +2913,28 @@ IARM_Result_t _dsSetAudioDucking(void *arg)
 
     IARM_BUS_Lock(lock);
     int volume = 0;
-    float volumeLevel = 0;
     bool portEnabled = false;
     dsAudioSetDuckingParam_t *param = (dsAudioSetDuckingParam_t *)arg;
     IARM_Bus_DSMgr_EventData_t eventData;
-    INT_DEBUG("%s action : %d type :%d val :%d \n",__FUNCTION__,param->action,param->type,param->level);
+    INT_INFO("%s action : %d type :%d val :%d m_LastVolumeLevel :%f  \n",__FUNCTION__,param->action,param->type,param->level,float(m_LastVolumeLevel));
 
     dsError_t ret = dsIsAudioPortEnabled(param->handle, &portEnabled);
     if (ret != dsERR_NONE) {
-        INT_INFO("%s failed dsIsAudioPortEnabled\n",__FUNCTION__);
+        INT_ERROR("%s failed dsIsAudioPortEnabled\n",__FUNCTION__);
     }
-
-    ret = dsGetAudioLevel (param->handle, &volumeLevel);
-    if (ret != dsERR_NONE) {
-        INT_ERROR("%s failed dsGetAudioLevel\n",__FUNCTION__);
-    }
-
-    INT_DEBUG("%s volumeLevel:%f \n",__FUNCTION__, volumeLevel);
 
     if(param->action == dsAUDIO_DUCKINGACTION_START)
     {
         m_isDuckingInProgress = true;
 	if(param->type == dsAUDIO_DUCKINGTYPE_RELATIVE )
 	{
-             volume = (volumeLevel * param->level) / 100;
+             volume = (m_LastVolumeLevel* param->level) / 100;
 	}
 	else
 	{
-           if(param->level > volumeLevel)
+           if(param->level > m_LastVolumeLevel)
            {
-		 volume =  volumeLevel;
+		 volume =  m_LastVolumeLevel;
 	   }
            else
 	   {
@@ -2951,7 +2945,7 @@ IARM_Result_t _dsSetAudioDucking(void *arg)
     else
     {
 	m_isDuckingInProgress = false;
-	volume = volumeLevel;
+	volume = (int)m_LastVolumeLevel;
     }
 
     if(m_MuteStatus || !portEnabled)
@@ -2962,7 +2956,7 @@ IARM_Result_t _dsSetAudioDucking(void *arg)
         return IARM_RESULT_SUCCESS;
     }
 
-    INT_DEBUG(":%s adjusted volume volume :%d m_volumeDuckingLevel :%d\n",__FUNCTION__,volume,m_volumeDuckingLevel );
+    INT_INFO(":%s adjusted volume volume :%d m_volumeDuckingLevel :%d\n",__FUNCTION__,volume,m_volumeDuckingLevel );
 
     // apply volume to hal layer
     dsAudioPortType_t _APortType = _GetAudioPortType(param->handle);
@@ -3000,7 +2994,7 @@ IARM_Result_t _dsSetAudioDucking(void *arg)
         else if (_APortType == dsAUDIOPORT_TYPE_HDMI) {
                 mode = _srv_HDMI_Audiomode;
         }
-        INT_DEBUG("The Port type is :%d  Audio Settings Mode is %d \r\n",_APortType, mode);
+        INT_INFO("The Port type is :%d  Audio Settings Mode is %d \r\n",_APortType, mode);
 
         if(mode == dsAUDIO_STEREO_PASSTHRU && volume != 100)
         {
@@ -3315,11 +3309,12 @@ static IARM_Result_t setAudioDuckingAudioLevel(intptr_t handle)
     if(m_isDuckingInProgress)
     {
          volume = m_volumeDuckingLevel;
+         INT_INFO("%s: audio level set to m_volumeDuckingLevel : %f", __FUNCTION__, volume);
     }
     else
     {
 	 volume = m_LastVolumeLevel;
-         INT_INFO("%s: audio level from cache before ducking started: %f\n", __FUNCTION__, volume);
+         INT_INFO("%s: audio level from cache before ducking started: %f", __FUNCTION__, volume);
     }
     if (dsSetAudioLevelFunc == 0)
     {
